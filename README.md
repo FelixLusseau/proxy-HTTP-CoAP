604ba44113d357055b059bd689b0dbef88965506
FelixLusseau <felix.lusseau@gmail.com>
2022-12-15 14:43:07 +0100

    readme and comments

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# proxy-HTTP-CoAP
+# Proxy HTTP-CoAP
+
+This is a simple proxy in Rust that forwards HTTP requests from internet on a Raspberry Pi 3 B to a CoAP server on a Arduino MKR 1010 through 2 ethernet subnetworks.
+
+<a href="https://www.rust-lang.org" target="_blank" rel="noreferrer"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Rust_programming_language_black_logo.svg/480px-Rust_programming_language_black_logo.svg.png" alt="rust" width="40" height="40"/></a>
+<a href="https://www.raspberrypi.com" target="_blank" rel="noreferrer"> <img src="https://www.raspberrypi.com/app/uploads/2022/02/COLOUR-Raspberry-Pi-Symbol-Registered.png" alt="raspberry" width="40" height="40"/> </a> 
+<a href="https://www.arduino.cc" target="_blank" rel="noreferrer"> <img src="https://cdn.worldvectorlogo.com/logos/arduino-1.svg" alt="arduino" width="40" height="40"/> </a> 
+
+Set the IP addresses of the Raspberry Pi and the Arduino in the `src/main.rs` file.
+Use `cargo run` to compile and run the proxy.
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::{
 };
 
 fn main() {
+    // Web server waiting for requests on port 8080
     let listener = TcpListener::bind("10.42.0.100:8080").unwrap();
 
     for stream in listener.incoming() {
@@ -18,6 +19,7 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
+    // Read the request
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
     let mut path = "/";
@@ -33,14 +35,15 @@ fn handle_connection(mut stream: TcpStream) {
         status_line = "HTTP/1.1 200 OK";
         path = "/light";
     } else {
+        // Return 404 if the path is not found upwards
         let contents = "404 NOT FOUND\r\n";
         let length = contents.len();
-        let response =
-            format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}\r\n\r\n");
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}\r\n");
         stream.write_all(response.as_bytes()).unwrap();
         return;
     }
 
+    // Send the request to the CoAP server on the Arduino
     let mut request: CoapRequest<SocketAddr> = CoapRequest::new();
 
     request.set_method(Method::Get);
@@ -53,11 +56,12 @@ fn handle_connection(mut stream: TcpStream) {
         .send_to(&packet[..], "10.43.0.217:5683")
         .expect("Could not send the data");
 
+    // Receive the response from the CoAP server
     let mut buf = [0; 1024];
     let mut contents = "";
     match socket.recv(&mut buf) {
         Ok(received) => {
-            contents = str::from_utf8(&buf[8..received]).unwrap();
+            contents = str::from_utf8(&buf[8..received]).unwrap(); // Save the response in a variable
             println!(
                 "received {received} bytes {:?}",
                 str::from_utf8(&buf[8..received]).unwrap()
@@ -66,6 +70,7 @@ fn handle_connection(mut stream: TcpStream) {
         Err(e) => println!("recv function failed: {e:?}"),
     }
 
+    // Send the HTTP response to the client
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}\r\n\r\n");
